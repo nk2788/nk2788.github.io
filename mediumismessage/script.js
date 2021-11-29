a64e6d5e10fd06a3ad6dc29d6e9be7f6e6fdbbf8
Nidhi Khettry <nidhikhettry@MacBook-Air.lan>
2021-11-28 22:45:27 -0500

    JLibrary

---
+++ mediumismessage/script.js
@@ -0,0 +1,7 @@
+let small = document.querySelector("inside");
+
+function enlargeImg(){
+	console.log("yg");
+	small.style.tranform="scale(1.5)";
+	small.style.transition = "transform 0.25s ease";
+}
